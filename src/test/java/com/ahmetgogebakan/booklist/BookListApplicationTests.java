104cf4d73a993defb50ca3457723d3aaea0bc87f
Ahmet GOGEBAKAN <ahmetgogebakan@Ahmets-MacBook-Pro.local>
2020-12-26 23:26:41 +0300

    initial commit

---
+++ src/test/java/com/ahmetgogebakan/booklist/BookListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetgogebakan.booklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
